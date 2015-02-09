ef08ac40b1b3cb8d0130b4577004afc7a99cd232
jatin <phpsoftsol@gmail.com>
2015-02-09 16:54:23 +0530

    login page sign up

--- www/js/app.js
+++ www/js/app.js
@@ -1,9 +1,18 @@
 	 var handleLoginForm = function () {
+            jQuery('.signup-form').hide();
 
         jQuery('#forget-password').click(function () {
             jQuery('.login-form').hide();
+            jQuery('.signup-form').hide();
             jQuery('.forget-form').show(200);
         });
+	    jQuery('#signup-btn').click(function () {
+            jQuery('.login-form').hide();
+            jQuery('.forget-form').hide();
+           jQuery('.signup-form').show(200);
+        });
+
+
 
         jQuery('#forget-btn, #back-btn').click(function () {
             jQuery('.login-form').slideDown(200);
@@ -12,7 +21,23 @@
     };
 	 
 
+$('#saveSignUp-btn').click(function(){
+console.log(	$('#signup_username').val() ) ;
+	// var formData= $('#signup-form')[0].serializeArray(); 
 
+	// console.log(formData);
+
+
+	var newUser= {};
+	newUser['username'] = $('#signup_username').val();
+	console.log(newUser);
+
+
+	/// after validate
+
+	LocalStorage.save('user',newUser); 
+
+})
 var App = {
 	init: function ()
 	{
